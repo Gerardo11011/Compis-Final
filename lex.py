17a64f79786a7a7b8f54bcb1a77318d77e7193a2
Oscar Guevara <guevara1697@gmail.com>
2019-10-17 23:40:52 -0500

    Modificación de Lex

--- lex.py
+++ lex.py
@@ -7,17 +7,22 @@ import sys
 
 # Lista de palabras reservadas.
 reserved = {
-    'if' : 'IF' ,
-    'else' : 'ELSE',
-    'float' : 'FLOAT',
-    'int' : 'INT',
-    'output' : 'OUTPUT',
-    'loop' : 'LOOP',
     'begin' : 'BEGIN',
     'end' : 'END',
-    'then' : 'THEN',
-    'char' : 'CHAR',
-    'input' : 'INPUT'
+    'main' : 'MAIN',
+    'if' : 'IF' ,
+    'else' : 'ELSE',
+    'loop' : 'LOOP',
+    'func' : 'FUNC',
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'string' : 'STRING',
+    'bool' : 'BOOL',
+    'input' : 'INPUT',
+    'output' : 'OUTPUT',
+    'return' : 'RETURN',
+    'true' : 'TRUE',
+    'false' : 'FALSE'
 }
 
 # Lista de Tokens.
@@ -38,7 +43,6 @@ tokens = [
 
     'SEMICOLON',
     'COMMA',
-    'DP',
     'LKEY',
     'RKEY',
     'LPAREN',
@@ -46,7 +50,7 @@ tokens = [
     'LCORCH',
     'RCORCH',
 
-    'CTE_CH',
+    'CTE_S',
     'CTE_I',
     'CTE_F',
 
@@ -73,7 +77,6 @@ t_NE = r'\<>'
 
 t_SEMICOLON = r'\;'
 t_COMMA = r'\,'
-t_DP = r'\:'
 t_LKEY = r'\{'
 t_RKEY = r'\}'
 t_LPAREN = r'\('
@@ -81,7 +84,7 @@ t_RPAREN = r'\)'
 t_LCORCH = r'\['
 t_RCORCH = r'\]'
 
-t_CTE_CH = r'\"([^\\\n]|(\\.))*?\"'
+t_CTE_S = r'\"([^\\\n]|(\\.))*?\"'
 
 tokens += list(reserved.values())
 
