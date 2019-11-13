0840d8840083f517f9607b6a66de5fcab9f99a99
Oscar Guevara <guevara1697@gmail.com>
2019-11-12 18:48:02 -0600

    Cambio push_cte

--- yacc.py
+++ yacc.py
@@ -413,8 +413,9 @@ def p_push_cte(p):
     memo.guardarDireUsada(p[-1], dir)
     # memo.memory_dir = memo.insertLocalTemp(temp)
     # memo.updateLoc1al(p[-1], memo.memory_dir, temp)
-    quad.pushCte(p[-1])
+    quad.pushCte(p[-1], dir, tipo)
     # quad.pushCte(temp)
+
 
 def p_condicion(p):
     '''
