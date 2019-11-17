359bc39f9d1ecb4d6e6a4fd06cc47eeec480d51f
Oscar Guevara <guevara1697@gmail.com>
2019-11-16 21:53:58 -0600

    Auto stash before merge of "master" and "origin/master"

--- yacc.py
+++ yacc.py
@@ -543,7 +543,7 @@ def p_funcionTres(p):
             master.contadorDatosPasados += 1
             # print("ENTRA", len(master.arrParam), p[1])
             if master.contadorDatosPasados > master.simbolos[master.miParamFunc].value["PARAMCANTI"].value:
-                print("Sobran parametros en la funcion", master.miParamFunc, "en el main.")
+                print("Sobran parametros en la funcion", master.miParamFunc, ".")
                 sys.exit()
             master.updateIdInFunc(master.arrParam[-1], master.miParamFunc, valor)
             del(master.arrParam[-1])
